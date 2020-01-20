08827303e691bbafc706b6bd138ba671e7f9c31b
waitwalker <waitwalker@163.com>
2020-01-20 10:55:26 +0800

    播放器视图

---
+++ WWPlayer/WWPlayer/WWPlayer.h
@@ -0,0 +1,17 @@
+//
+//  WWPlayer.h
+//  WWPlayer
+//
+//  Created by etiantian on 2020/1/20.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WWPlayer : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WWPlayer/WWPlayer/WWPlayer.m
@@ -0,0 +1,20 @@
+//
+//  WWPlayer.m
+//  WWPlayer
+//
+//  Created by etiantian on 2020/1/20.
+//  Copyright © 2020 etiantian. All rights reserved.
+//
+
+#import "WWPlayer.h"
+
+@implementation WWPlayer
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        self.backgroundColor = [UIColor redColor];
+    }
+    return self;
+}
+
+@end
